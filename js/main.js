d861152d3e76560d576a258ab5aef55848cd4ff9
KirillEvgen <churkin.ke@gmail.com>
2026-01-18 16:44:58 +0500

    registr

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Smooth scroll for "To Top" button
+document.addEventListener('DOMContentLoaded', function() {
+    // Button click handler is in HTML onclick attribute
+    // This file can be used for additional functionality if needed
+});
